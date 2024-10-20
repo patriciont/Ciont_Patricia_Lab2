92e517cbce95a2a235c307bdf45239c2c72af6f2
danac <danac@DESKTOP-Q2L29OG>
2024-10-20 10:49:27 +0300

    Add project files.

---
+++ Data/Ciont_Patricia_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ciont_Patricia_Lab2.Models;
+
+namespace Ciont_Patricia_Lab2.Data
+{
+    public class Ciont_Patricia_Lab2Context : DbContext
+    {
+        public Ciont_Patricia_Lab2Context (DbContextOptions<Ciont_Patricia_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ciont_Patricia_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Ciont_Patricia_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Ciont_Patricia_Lab2.Data;
+using Ciont_Patricia_Lab2.Models;
+
+namespace Ciont_Patricia_Lab2.Pages.Books
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Ciont_Patricia_Lab2.Data.Ciont_Patricia_Lab2Context _context;
+
+        public CreateModel(Ciont_Patricia_Lab2.Data.Ciont_Patricia_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID",
+"PublisherName");
+            return Page();
+        }
+
+        [BindProperty]
+        public Book Book { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Book.Add(Book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
